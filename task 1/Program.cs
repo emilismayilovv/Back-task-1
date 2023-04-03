18c4fa8da14b7b11b3fb7cf3f00bfc9eaa7a529c
emilismayilovv <emilisma2002@mail.ru>
2023-04-04 01:31:59 +0400

    BACK Task1

---
+++ task 1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+
+namespace task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int[] array = { 1, 2, 3, 4, 5 };
+
+            int[] reverse = new int [array.Length];
+
+            for (int i =array.Length - 1; i >= 0; i--)
+            {
+                reverse[array.Length - i - 1] = array[i];
+            }
+            Console.WriteLine(String.Join(" ", reverse));*/
+
+
+
+
+            /* int[] array = { 1, 22, 3, 43,9,4};
+             int count = 0;
+
+             for(int i=0; i<array.Length; i++)
+             {
+
+                 if (array[i]<10 && array[i] >= 0)
+                 {
+                     //Console.WriteLine(array[i]);
+                     count++;
+                 }
+
+
+             }
+             Console.WriteLine(count);*/
+
+
+
+
+            /* int lowEnd = 0;
+             int highEnd = 10;
+             for (int i = lowEnd; i <= highEnd; i++)
+             {   
+                 if (i % 3 == 0 && i!=0)
+                 {
+                     Console.WriteLine(i);
+                 }
+             }*/
+
+
+
+
+
+            /*int num = 12;
+            int count = 0;
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    //Console.WriteLine(i);
+                    count++;
+
+                }
+
+
+            }
+            Console.WriteLine(count);*/
+
+
+        }
+    }
+}
